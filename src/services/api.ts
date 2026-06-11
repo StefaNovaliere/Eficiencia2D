bca2e4d4c649ebd1556cb4b699541f6bd4e75bdd
Brenda Tapari <brendatapa6@gmail.com>
2026-06-11 19:22:53 -0300

    cambio en el puerto

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = process.env.VITE_API_URL ;
+
+const API_BASE_URL = import.meta.env.VITE_API_URL
 import { decodePackedFaces } from "@/core/packed-faces";
 export interface UploadResponse {
   message: string;
