469182cf3a52bee1f44ecf7d0a11ecfccd729500
Brenda Tapari <brendatapa6@gmail.com>
2026-06-11 19:27:19 -0300

    Correcciones de variables de entorno

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 
-const API_BASE_URL = import.meta.env.VITE_API_URL
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL
 import { decodePackedFaces } from "@/core/packed-faces";
 export interface UploadResponse {
   message: string;
